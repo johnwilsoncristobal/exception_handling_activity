32805ee31bcf86977fa9afffd2c4f8bf32bc9547
John Wilson Cristobal <johnwilsoncristobal@gmail.com>
2026-05-08 22:16:46 +0800

    added Addition subclass

--- simple_calculator.py
+++ simple_calculator.py
@@ -2,3 +2,8 @@
 class Operation:
     def calculate(self, first_number, second_number):
         raise NotImplementedError
+
+# Subclasses for each operation
+class Addition(Operation):
+    def calculate(self, first_number, second_number):
+        return first_number + second_number
